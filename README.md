1e49877fdde360d1390674ef62ec33e275dcc915
theouternet <jrougeau@gmail.com>
2020-12-13 21:58:03 -0600

    JS Get

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # QUOTr
 
+This source code is the sole property of AstonishSoft, and may not be used without permission.
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function displayInfo() {
-
+    const info = JSON.parse(this.responseText);
+    
 }
 
 function getInfo() {
-    
-}
+    const req = new XMLHttpRequest();
+    const symbol = document.getElementById('symbol').value;
+    req.addEventListener('load', displayInfo);
+    req.open('GET', 'https://financialmodelingprep.com/api/v3/company/profile/' + symbol + '?apikey=df221fd64469296f1ce8157078a0dca6');
+    req.send();
+  }
