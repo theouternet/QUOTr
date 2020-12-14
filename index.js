7f09ea8d89cd5c6fc3bccf8a47bb238ffdd73004
theouternet <jrougeau@gmail.com>
2020-12-13 22:55:14 -0600

    Conditional Rendering

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function displayInfo() {
     const info = JSON.parse(this.responseText);
-
+  
     let symbol = info.symbol
-
+  
     let image = info.profile.image
+  
     let elem = document.createElement("img");
     elem.src = image;
+  
     let companyName = info.profile.companyName
     let industry = info.profile.industry
     let price = info.profile.price
@@ -13,20 +15,38 @@ function displayInfo() {
     let changesPercentage = info.profile.changesPercentage
     let isNegative = null
     let website = info.profile.website
-
+  
+  
     document.getElementById('symbol').innerHTML = symbol;
-
+  
     document.getElementById("logo").appendChild(elem);
+    
     document.getElementById('company-name').innerHTML = companyName;
     document.getElementById('industry').innerHTML = industry;
     document.getElementById('price').innerHTML = "$" + price;
     document.getElementById('changes').innerHTML = "$" + changes;
     document.getElementById('changes-percentage').innerHTML = changesPercentage;
     document.getElementById('website').innerHTML = "<a href='"+website+"'>" + website + "</a>";
-
-}
-
-function getInfo() {
+  
+    if (changes < 0) {
+      isNegative = true
+    }
+    else {
+      isNegative = false
+    }
+  
+    if (isNegative == true) {
+      document.getElementById('changes').classList.add('red')
+      document.getElementById('changes-percentage').classList.add('red')
+    }
+    else {
+      document.getElementById('changes').classList.add('green')
+      document.getElementById('changes-percentage').classList.add('green')
+    }
+  
+  }
+  
+  function getInfo() {
     const req = new XMLHttpRequest();
     const symbol = document.getElementById('symbol').value;
     req.addEventListener('load', displayInfo);
